b3b57bc13628f3d56bd3cc2824471cecf656b6c9
Patryk Mech <PatrykAdamMech@gmail.com>
2022-02-07 15:01:40 +0100

    Finding agree button and clicking it.

--- src/test/java/com/example/junittdd/SeleniumChromeTest.java
+++ src/test/java/com/example/junittdd/SeleniumChromeTest.java
@@ -1,4 +1,7 @@
+package com.example.junittdd;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -6,12 +9,15 @@ public class SeleniumChromeTest {
     @Test
     void seleniumChromeTest() {
         System.setProperty("webdriver.chrome.driver","D:\\Gierki\\Szkolenie\\Testing\\chromedriver\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+        ChromeDriver driver;
 
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
 
         driver.get("https://google.com");
+
+        WebElement agreeButton = driver.findElementById("L2AGLb");
+        agreeButton.click();
 
         driver.quit();
     }
